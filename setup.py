6514759c1e900e9ad47e77465c82e6530182be8a
Steve Milner <stevem@gnulinux.net>
2014-02-21 15:38:27 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''
+Build script.
+'''
+import os.path
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cacophony',
+    version='0.0.1',
+    author='Steve Milner',
+    url='https://github.com/RHInception/cacophony',
+    license='AGPLv3',
+    zip_safe=False,
+    package_dir={
+        'cacophony': os.path.join('src', 'cacophony')
+    },
+    packages=find_packages('src'),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python'],
+
+)
